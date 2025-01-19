6a3efc40a916051b80e5309e20e4e94c7304ac93
Manish Choudhary <choudharymanish@protonmail.com>
2025-01-19 15:02:24 +0000

    Solved more problems

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+    let len = str.length - 1
+    let result = ''
+    for(let i=len; i >=0; i-- ) {
+        result += str[i]
+    }
+
+    return result
+}
 
 module.exports = reverseString;
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,21 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+   
+    str = str.toLowerCase()
+    str = str.replace(/[^a-z0-9]/g,"")
+    let [left,right ]= [0, str.length -1]
+   
+    let result = true
+    while(left<=right) {
+        if(str[left]!=str[right]) {
+            result = false
+            break
+        } else {
+            left++
+            right--
+        }
+    }
+
+    return result
+}
 
 module.exports = isPalindrome;
